8376e1a4a4e410f7841ccd9a46eeca03af78bdcc
Carmelo7BS <114226997+Carmelo7BS@users.noreply.github.com>
2023-01-20 23:27:22 +0100

    modified

--- Index.js
+++ Index.js
@@ -4,9 +4,13 @@ const generateCsb = document.getElementById('generate-csb')
 const generateTdg = document.getElementById('generate-tdg')
 const generateSdc = document.getElementById('generate-sdc')
 const generateLbt = document.getElementById('generate-lbt')
+const central = document.getElementById('central-button')
 
+const genReps = document.getElementById('gen-reps')
+const genTimer = document.getElementById('gen-timer')
+//Elements variables
+const ulEl = document.getElementById('list')
 //Exercise variable
-const ulEl =document.getElementById('list')
 const shoulder = ['Military-press', 'Alzate laterali', 'Alzate in alto', 'Alzate Frontali', 'Tirate al mento']
 const chest = ['Push Ups', 'Croci a terra', 'Croci ai cavi', 'Spinte da terra', 'Pull over petto']
 const bicepts = ['Curl Hummer alternato', 'Curl manubri', 'Curl presa inversa', 'Hummer ai cavi']
@@ -14,16 +18,39 @@ const tricepts = ['Estensione tricipite manubrio', 'French press', 'Tricipite pu
 const dorsali = ['Volare prona', 'Rematore', 'Rematore presa inversa', 'Pulley basso', 'Scrollate spalle', 'Pull over dorsali']
 const gambe = ['Squat', 'Squat bulgari', 'Calf', 'Sumo squat', 'Stacchi rumeni', 'Gluteus bridge', 'Affondi']
 const addominali =  ['Crunch', 'Leg Raise', 'Bicicletta', 'L-toe touch', 'Isometria barchetta', 'Russian twist', 'Crunch incrociato', 'Crunch con gambe sollevate']
-const fullbody = ['Plank', 'Plank basso', 'Plank inverso']
-const elementEl = document.getElementById('element')
+const fullbody = ['Plank', 'Plank basso', 'Plank inverso', 'Plank tocco spalla', 'Plank pugno']
+//Reps variables
+const reps = ['Reps: 3 x 10', 'Reps: 4 x 8', 'Reps: 3 x 12']
+
 let numbExercise = 1
 
+
 //Functions
+//GenerateReps
+genReps.addEventListener('click', function(){
+  genReps.value = ''
+  let randomRep = Math.floor(Math.random()*reps.length)
+  genReps.value += reps[randomRep]
+})
+
+//GenerateTimer
+let start = genTimer.addEventListener('click', function start(){
+  let time = 40
+  let countdown = setInterval(function(){
+    if (time <= 0){
+      clearInterval(countdown)
+      genTimer.value = 'Timer'
+    }
+    genTimer.value = time
+    time--
+  }, 1000)
+})
+
+
 //1-Full body
-generateEl.addEventListener('click', function(){
+generateEl.addEventListener('click', function (){
   let warmUp = `<li class="liFissi">Warm up</li>`
   let stretching = `<li class="liFissi">Stretching</li>`
-  let ripetizioni = `Reps: 3 X 10`
   let listItems = ""
   for(let i = 0; i < numbExercise; i++){
     let randomShoulder = Math.floor(Math.random()*shoulder.length)
@@ -44,15 +71,40 @@ generateEl.addEventListener('click', function(){
                   `<li>${fullbody[randomFullbody]}</li>`
   }
   let workout = warmUp + listItems + stretching
-  elementEl.textContent = ripetizioni
   ulEl.innerHTML = workout
+  central.style.display = 'inline'
+  central.textContent = 'Full Body'
+  central.addEventListener('click', function (){
+    let warmUp = `<li class="liFissi">Warm up</li>`
+    let stretching = `<li class="liFissi">Stretching</li>`
+    let listItems = ""
+    for(let i = 0; i < numbExercise; i++){
+      let randomShoulder = Math.floor(Math.random()*shoulder.length)
+      let randomChest = Math.floor(Math.random()*chest.length)
+      let randomBicepts = Math.floor(Math.random()*bicepts.length)
+      let randomTricepts = Math.floor(Math.random()*tricepts.length)
+      let randomDorsali = Math.floor(Math.random()*dorsali.length)
+      let randomGambe = Math.floor(Math.random()*gambe.length)
+      let randomAddominali = Math.floor(Math.random()*addominali.length)
+      let randomFullbody = Math.floor(Math.random()*fullbody.length)
+      listItems += `<li>${shoulder[randomShoulder]}</li>` +
+                    `<li>${chest[randomChest]}</li>` +
+                    `<li>${bicepts[randomBicepts]}</li>` +
+                    `<li>${tricepts[randomTricepts]}</li>` +
+                    `<li>${dorsali[randomDorsali]}</li>` +
+                    `<li>${gambe[randomGambe]}</li>` +
+                    `<li>${addominali[randomAddominali]}</li>` +
+                    `<li>${fullbody[randomFullbody]}</li>`
+    }
+    let workout = warmUp + listItems + stretching
+    ulEl.innerHTML = workout
+  })
 })
 
 //2-Chest-Shoulder-bicepts
 generateCsb.addEventListener('click', function(){
   let warmUp = `<li class="liFissi">Warm up</li>`
   let stretching = `<li class="liFissi">Stretching</li>`
-  let ripetizioni = `Reps: 3 X 10`
   let listItems = ""
   let numbEx = numbExercise + 1
   for(let i = 0; i < numbEx; i++){
@@ -66,15 +118,33 @@ generateCsb.addEventListener('click', function(){
                   `<li>${addominali[randomAddominali]}</li>`
   }
   let workout = warmUp + listItems + stretching
-  elementEl.textContent = ripetizioni
   ulEl.innerHTML = workout
+  central.style.display = 'inline'
+  central.textContent = 'Petto/Spalle/Bicipiti'
+  central.addEventListener('click', function(){
+    let warmUp = `<li class="liFissi">Warm up</li>`
+    let stretching = `<li class="liFissi">Stretching</li>`
+    let listItems = ""
+    let numbEx = numbExercise + 1
+    for(let i = 0; i < numbEx; i++){
+      let randomShoulder = Math.floor(Math.random()*shoulder.length)
+      let randomChest = Math.floor(Math.random()*chest.length)
+      let randomBicepts = Math.floor(Math.random()*bicepts.length)
+      let randomAddominali = Math.floor(Math.random()*addominali.length)
+      listItems += `<li>${shoulder[randomShoulder]}</li>` +
+                    `<li>${chest[randomChest]}</li>` +
+                    `<li>${bicepts[randomBicepts]}</li>` +
+                    `<li>${addominali[randomAddominali]}</li>`
+    }
+    let workout = warmUp + listItems + stretching
+    ulEl.innerHTML = workout
+  })
 })
 
 //3-Tricepts-Dorsali-gambe
 generateTdg.addEventListener('click', function(){
   let warmUp = `<li class="liFissi">Warm up</li>`
   let stretching = `<li class="liFissi">Stretching</li>`
-  let ripetizioni = `Reps: 3 X 10`
   let listItems = ""
   let numbEx = numbExercise + 1
   for(let i = 0; i < numbEx; i++){
@@ -90,15 +160,35 @@ generateTdg.addEventListener('click', function(){
   listItems += `<li>${addominali[randomAddominali]}</li>` +
                `<li>${fullbody[randomFullbody]}</li>`
   let workout = warmUp + listItems + stretching
-  elementEl.textContent = ripetizioni
   ulEl.innerHTML = workout
+  central.style.display = 'inline'
+  central.textContent = 'Tricipiti/Dorsali/Gambe'
+  central.addEventListener('click', function(){
+    let warmUp = `<li class="liFissi">Warm up</li>`
+    let stretching = `<li class="liFissi">Stretching</li>`
+    let listItems = ""
+    let numbEx = numbExercise + 1
+    for(let i = 0; i < numbEx; i++){
+      let randomTricepts = Math.floor(Math.random()*tricepts.length)
+      let randomDorsali = Math.floor(Math.random()*dorsali.length)
+      let randomGambe = Math.floor(Math.random()*gambe.length)
+      listItems += `<li>${tricepts[randomTricepts]}</li>` +
+                   `<li>${dorsali[randomDorsali]}</li>` +
+                   `<li>${gambe[randomGambe]}</li>`
+    }
+    let randomAddominali = Math.floor(Math.random()*addominali.length)
+    let randomFullbody = Math.floor(Math.random()*fullbody.length)
+    listItems += `<li>${addominali[randomAddominali]}</li>` +
+                 `<li>${fullbody[randomFullbody]}</li>`
+    let workout = warmUp + listItems + stretching
+    ulEl.innerHTML = workout
+  })
 })
 
 //4-Shoulder-Dorsali-chest
 generateSdc.addEventListener('click', function(){
   let warmUp = `<li class="liFissi">Warm up</li>`
   let stretching = `<li class="liFissi">Stretching</li>`
-  let ripetizioni = `Reps: 3 X 10`
   let listItems = ""
   let numbEx = numbExercise + 1
   for(let i = 0; i < numbEx; i++){
@@ -114,15 +204,35 @@ generateSdc.addEventListener('click', function(){
   let randomAddominali = Math.floor(Math.random()*addominali.length)
   listItems += `<li>${addominali[randomAddominali]}</li>`
   let workout = warmUp + listItems + stretching
-  elementEl.textContent = ripetizioni
   ulEl.innerHTML = workout
+  central.style.display = 'inline'
+  central.textContent = 'Spalle/Dorsali/Petto'
+  central.addEventListener('click', function(){
+    let warmUp = `<li class="liFissi">Warm up</li>`
+    let stretching = `<li class="liFissi">Stretching</li>`
+    let listItems = ""
+    let numbEx = numbExercise + 1
+    for(let i = 0; i < numbEx; i++){
+      let randomShoulder = Math.floor(Math.random()*shoulder.length)
+      let randomDorsali = Math.floor(Math.random()*dorsali.length)
+      let randomChest = Math.floor(Math.random()*chest.length)
+      let randomFullbody = Math.floor(Math.random()*fullbody.length)
+      listItems += `<li>${shoulder[randomShoulder]}</li>` +
+                   `<li>${dorsali[randomDorsali]}</li>` +
+                   `<li>${chest[randomChest]}</li>` +
+                   `<li>${fullbody[randomFullbody]}</li>`
+    }
+    let randomAddominali = Math.floor(Math.random()*addominali.length)
+    listItems += `<li>${addominali[randomAddominali]}</li>`
+    let workout = warmUp + listItems + stretching
+    ulEl.innerHTML = workout
+  })
 })
 
 //5-Legs-Bicepts-tricepts
 generateLbt.addEventListener('click', function(){
   let warmUp = `<li class="liFissi">Warm up</li>`
   let stretching = `<li class="liFissi">Stretching</li>`
-  let ripetizioni = `Reps: 3 X 10`
   let listItems = ""
   let numbEx = numbExercise + 1
   for(let i = 0; i < numbEx; i++){
@@ -138,6 +248,27 @@ generateLbt.addEventListener('click', function(){
   let randomFullbody = Math.floor(Math.random()*fullbody.length)
   listItems += `<li>${fullbody[randomFullbody]}</li>`
   let workout = warmUp + listItems + stretching
-  elementEl.textContent = ripetizioni
   ulEl.innerHTML = workout
+  central.style.display = 'inline'
+  central.textContent = 'Gambe/Bicipiti/Tricipiti'
+  central.addEventListener('click', function(){
+    let warmUp = `<li class="liFissi">Warm up</li>`
+    let stretching = `<li class="liFissi">Stretching</li>`
+    let listItems = ""
+    let numbEx = numbExercise + 1
+    for(let i = 0; i < numbEx; i++){
+      let randomGambe = Math.floor(Math.random()*gambe.length)
+      let randomBicepts = Math.floor(Math.random()*bicepts.length)
+      let randomTricepts = Math.floor(Math.random()*tricepts.length)
+      let randomAddominali = Math.floor(Math.random()*addominali.length)
+      listItems += `<li>${gambe[randomGambe]}</li>` +
+                   `<li>${bicepts[randomBicepts]}</li>` +
+                   `<li>${tricepts[randomTricepts]}</li>` +
+                   `<li>${addominali[randomAddominali]}</li>`
+    }
+    let randomFullbody = Math.floor(Math.random()*fullbody.length)
+    listItems += `<li>${fullbody[randomFullbody]}</li>`
+    let workout = warmUp + listItems + stretching
+    ulEl.innerHTML = workout
+  })
 })
